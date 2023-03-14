9935fe9cc6546256f540729068ec6ca3cdedb78d
Sean O <ogradysean51@gmail.com>
2023-03-14 09:36:24 -0400

    Record Overhaul
    Now creates constant spectrograms
    Lots of print statements to clear up
    + pycache change

--- Record.py
+++ Record.py
@@ -1,48 +1,112 @@
 import pyaudio
-import wave
-import os
+from queue import Queue
+from threading import Thread
+import sys
+import time
+import numpy as np
+import matplotlib.mlab as mlab
+import numpy as np
+import time
+from pydub import AudioSegment
+import sys
+from scipy.io.wavfile import write
 
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 512
-RECORD_SECONDS = 0.5
-WAVE_OUTPUT_FILENAME = "./Process-Segments/"
-
-INDEX = 0
-BOTTOM = 0
 
 
-def record(device_index):
-    global INDEX
-    audio = pyaudio.PyAudio()
-    stream = audio.open(format=FORMAT, channels=CHANNELS,
-                        rate=RATE, input=True, input_device_index=0,
-                        frames_per_buffer=CHUNK)
-    Recordframes = []
+chunk_duration = 0.5 # Each read length in seconds from mic.
+fs = 48000 # sampling rate for mic
+chunk_samples = int(fs * chunk_duration) # Each read length in number of samples.
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        Recordframes.append(data)
+# Each model input data duration in seconds, need to be an integer numbers of chunk_duration
+feed_duration = 10
+feed_samples = int(fs * feed_duration)
 
+assert feed_duration/chunk_duration == int(feed_duration/chunk_duration)
+
+# Queue to communiate between the audio callback and main thread
+q = Queue()
+
+run = True
+
+silence_threshold = 10
+
+# Run the demo for a timeout seconds
+timeout = time.time() + 0.5*60  # 0.5 minutes from now
+
+# Data buffer for the input wavform
+data = np.zeros(feed_samples, dtype='int16')
+
+
+def get_spectrogram(data):
+    """
+    Function to compute a spectrogram.
+    
+    Argument:
+    predictions -- one channel / dual channel audio data as numpy array
+
+    Returns:
+    pxx -- spectrogram, 2-D array, columns are the periodograms of successive segments.
+    """
+    nfft = 200 # Length of each window segment
+    fs = 8000 # Sampling frequencies
+    noverlap = 120 # Overlap between windows
+    nchannels = data.ndim
+    if nchannels == 1:
+        pxx, _, _ = mlab.specgram(data, nfft, fs, noverlap = noverlap)
+    elif nchannels == 2:
+        pxx, _, _ = mlab.specgram(data[:,0], nfft, fs, noverlap = noverlap)
+    return pxx
+
+def get_audio_input_stream(callback):
+    stream = pyaudio.PyAudio().open(
+        format=pyaudio.paInt16,
+        channels=1,
+        rate=fs,
+        input=True,
+        frames_per_buffer=chunk_samples,
+        input_device_index=0,
+        stream_callback=callback)
+    return stream
+
+
+def callback(in_data, frame_count, time_info, status):
+    global run, timeout, data, silence_threshold    
+    if time.time() > timeout:
+        run = False        
+    data0 = np.frombuffer(in_data, dtype='int16')
+    if np.abs(data0).mean() < silence_threshold:
+        sys.stdout.write('-')
+        return (in_data, pyaudio.paContinue)
+    else:
+        sys.stdout.write('.')
+    data = np.append(data,data0)    
+    if len(data) > feed_samples:
+        data = data[-feed_samples:]
+        # Process data async by sending a queue.
+        q.put(data)
+    return (in_data, pyaudio.paContinue)
+
+
+print("initializing...")
+stream = get_audio_input_stream(callback)
+stream.start_stream()
+print("INITIALIZED \n ----------------------")
+
+try:
+    print("Trying to run")
+    while run:
+        print("Running")
+        data = q.get()
+        spectrum = get_spectrogram(data)
+        print(spectrum)
+        print("^^^ Spectrum ^^^")
+except (KeyboardInterrupt, SystemExit):
+    print("SystemExit")
     stream.stop_stream()
     stream.close()
-    audio.terminate()
-    file = WAVE_OUTPUT_FILENAME + "analyze" + str(INDEX) + ".wav"
-    INDEX += 1
-    if(INDEX > 5):
-        INDEX = 0
-    waveFile = wave.open(file, 'wb')
-    waveFile.setnchannels(CHANNELS)
-    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-    waveFile.setframerate(RATE)
-    waveFile.writeframes(b''.join(Recordframes))
-    waveFile.close() 
-
-def reset():
-    global BOTTOM
-    #os.remove("./Process-Segments/analyze" + str(BOTTOM) + ".wav")
-    BOTTOM += 1
+    timeout = time.time()
+    run = False
     
-    if(BOTTOM > 5):
-        BOTTOM = 0
+print("stopping")
+stream.stop_stream()
+stream.close()
